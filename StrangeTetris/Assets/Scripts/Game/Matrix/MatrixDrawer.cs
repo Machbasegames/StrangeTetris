541e328f9be75d3bf9a670e243e3fcebca52078d
devil-k <ehdgur0812@gmail.com>
2022-01-05 21:15:33 +0900

    add block type, and move function

--- StrangeTetris/Assets/Scripts/Game/Matrix/MatrixDrawer.cs
+++ StrangeTetris/Assets/Scripts/Game/Matrix/MatrixDrawer.cs
@@ -21,18 +21,18 @@ public class Block
                                          0f);
 
         spriteRenderer = transform.GetComponentInChildren<SpriteRenderer>();
-        SetSpriteEnable(false);
+        SetSpriteLevel(0);
     }
 
-    public void SetSpriteEnable(bool _isSpriteEnable) {
-        //spriteRenderer.enabled = _isSpriteEnable;
-        
+    public void SetSpriteLevel(int _spriteLevel) {
         Color _color = spriteRenderer.color;
 
-        if(_isSpriteEnable == true) {
-            _color.a = 1f;
-        } else {
-            _color.a = 0.5f;
+        if(_spriteLevel == 1) {
+            _color.a = 1.0f;
+        } else if(_spriteLevel == 2) {
+            _color.a = 0.6f;
+        } else if(_spriteLevel == 0) {
+            _color.a = 0.3f;
         }
 
         spriteRenderer.color = _color;
@@ -43,33 +43,63 @@ public class MatrixDrawer : MonoBehaviour
 {
     public GameObject blockPrefab;
 
-    public int matrixSizeY = 10;
+    public int matrixSizeY = 20;
     public int matrixSizeX = 10;
 
     Block[,] blockMatrix;
     int[,] matrix;
+    int[,] shadowMatrix;
+    bool isGenerate;
+
+    int[,,] blockSet = new int[7, 4, 4] {
+        { {0, 2, 2, 0},
+          {0, 2, 2, 0},
+          {0, 0, 0, 0},
+          {0, 0, 0, 0} },
+        { {0, 2, 0, 0},
+          {0, 2, 0, 0},
+          {0, 2, 2, 0},
+          {0, 0, 0, 0} },
+        { {0, 0, 2, 0},
+          {0, 2, 2, 0},
+          {0, 0, 2, 0},
+          {0, 0, 0, 0} },
+        { {0, 0, 2, 0},
+          {0, 0, 2, 0},
+          {0, 2, 2, 0},
+          {0, 0, 0, 0} },
+        { {0, 2, 2, 0},
+          {2, 2, 0, 0},
+          {0, 0, 0, 0},
+          {0, 0, 0, 0} },
+        { {2, 2, 0, 0},
+          {0, 2, 2, 0},
+          {0, 0, 0, 0},
+          {0, 0, 0, 0} },
+        { {0, 2, 0, 0},
+          {0, 2, 0, 0},
+          {0, 2, 0, 0},
+          {0, 2, 0, 0} },
+    };
 
     private void Awake() {
         blockMatrix = new Block[matrixSizeY, matrixSizeX];
         matrix = new int[matrixSizeY, matrixSizeX];
+        shadowMatrix = new int[matrixSizeY, matrixSizeX];
 
-        int _indexY = 0;
-        int _indexX = 0;
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                Transform _newBlock = GameObject.Instantiate<GameObject>(blockPrefab,
+                                                                        Vector3.zero,
+                                                                        Quaternion.identity,
+                                                                        transform).transform;
 
-        for(int i = 0; i < matrix.Length; i++) {
-            _indexY = i / matrixSizeY;
-            _indexX = i % matrixSizeX;
-
-            Transform _newBlock = GameObject.Instantiate<GameObject>(blockPrefab,
-                                                                     Vector3.zero,
-                                                                     Quaternion.identity,
-                                                                     transform).transform;
-
-            blockMatrix[_indexY, _indexX] = new Block(_newBlock,
-                                                      _indexY,
-                                                      _indexX,
-                                                      matrixSizeY,
-                                                      matrixSizeX);
+                blockMatrix[y, x] = new Block(_newBlock,
+                                                        y,
+                                                        x,
+                                                        matrixSizeY,
+                                                        matrixSizeX);
+            }
         }
     }
 
@@ -78,20 +108,83 @@ public class MatrixDrawer : MonoBehaviour
     {
         StartCoroutine(AutoGenerator());
         StartCoroutine(MatrixProcess());
+
+        isGenerate = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            MoveMatrix(-1);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+            MoveMatrix(1);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            DownEnd();
+        }
 
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            GenerateBlock();
+        }
+    }
+
+    private void LateUpdate() {
+        DrawMatrix();
+    }
+
+    void MoveMatrix(int _direction) {
+        int[,] _shadowMatrix = new int[matrixSizeY, matrixSizeX];
+
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                if(shadowMatrix[y, x] == 2) {
+                    if(((x + _direction) == matrixSizeX) || ((x + _direction) == -1)) {
+                        return ;
+                    }
+                    _shadowMatrix[y, x + _direction] = shadowMatrix[y, x];
+                }
+            }
+        }
+
+        if(IsCollisionMatrix(ref _shadowMatrix) == true) {
+            return ;
+        }
+
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                shadowMatrix[y, x] = _shadowMatrix[y, x];
+            }
+        }
+    }
+
+    void GenerateBlock() {
+        int _index = Random.Range(0, 7);
+        int _offsetX = Random.Range(0, matrixSizeX-4);
+
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                shadowMatrix[y, x] = 0;
+            }
+        }
+
+        for(int y = 0; y < 4; y++) {
+            for(int x = 0; x < 4; x++) {
+                shadowMatrix[y, x + _offsetX] += blockSet[_index, y, x];
+            }
+        }
     }
 
     IEnumerator AutoGenerator() {
         while(true) {
-            yield return new WaitForSeconds(1f);
+            yield return null;
 
-            for(int i = 0; i < Random.Range(1, 4); i++) {
-                matrix[0, Random.Range(0, matrixSizeX)] = 1;
+            if(isGenerate == true) {
+                yield return new WaitForSeconds(1f);
+
+                GenerateBlock();
+                isGenerate = false;
             }
         }
     }
@@ -99,21 +192,78 @@ public class MatrixDrawer : MonoBehaviour
     void DrawMatrix() {
         for(int y = 0; y < matrixSizeY; y++) {
             for(int x = 0; x < matrixSizeX; x++) {
-                blockMatrix[y, x].SetSpriteEnable(false);
-
-                if(matrix[y, x] == 1) {
-                    blockMatrix[y, x].SetSpriteEnable(true);
+                blockMatrix[y, x].SetSpriteLevel(matrix[y, x]);
+                
+                if(shadowMatrix[y, x] == 2) {
+                    blockMatrix[y, x].SetSpriteLevel(2);
                 }
             }
         }
     }
 
+    void FixMatrix() {
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                if(shadowMatrix[y, x] == 2) {
+                    matrix[y, x] = 1;
+                    shadowMatrix[y, x] = 0;
+                }
+            }
+        }
+
+        CalculateMatrix();
+
+        isGenerate = true;
+    }
+
+    bool IsCollisionMatrix(ref int[,] _shadowMatrix) {
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                if(matrix[y, x] + _shadowMatrix[y, x] == 3) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     void DownRow() {
+        int[,] _shadowMatrix = new int[matrixSizeY, matrixSizeX];
+
+        for(int y = 0; y < matrixSizeY; y++) {
+            for(int x = 0; x < matrixSizeX; x++) {
+                _shadowMatrix[y, x] = shadowMatrix[y, x];
+            }
+        }
+
+        DownAllRow(ref _shadowMatrix);
+        if(IsCollisionMatrix(ref _shadowMatrix) == true) {
+            FixMatrix();
+            return ;
+        }
+
+        DownAllRow(ref shadowMatrix);
+        for(int x = 0; x < matrixSizeX; x++) {
+            if(shadowMatrix[matrixSizeY - 1, x] != 0) {
+                FixMatrix();
+                return ;
+            }
+        }
+    }
+
+    void DownEnd() {
+        while(isGenerate == false) {
+            DownRow();
+        }
+    }
+
+    void DownAllRow(ref int[,] _matrix) {
         for(int y = matrixSizeY - 1; y > 0; y--) {
             for(int x = 0; x < matrixSizeX; x++) {
-                if(matrix[y, x] != 1) {
-                    matrix[y, x] = matrix[y-1, x];
-                    matrix[y-1, x] = 0;
+                if(_matrix[y, x] == 0) {
+                    _matrix[y, x] = _matrix[y-1, x];
+                    _matrix[y-1, x] = 0;
                 }
             }
         }
@@ -135,15 +285,15 @@ public class MatrixDrawer : MonoBehaviour
                 for(int x = 0; x < matrixSizeX; x++) {
                     matrix[y, x] = 0;
                 }
+
+                DownAllRow(ref matrix);
             }
         }
     }
 
     IEnumerator MatrixProcess() {
         while(true) {
-            DrawMatrix();
             DownRow();
-            CalculateMatrix();
 
             yield return new WaitForSeconds(1f);
         }
